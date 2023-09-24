4b5e745c9e938d6d9d43649241845b88e635ce23
Amidu99 <amidushamika@gmail.com>
2023-09-24 11:31:14 +0530

    complete Case-01

--- Case-01/assets/js/script.js
+++ Case-01/assets/js/script.js
@@ -1,26 +1,35 @@
-var current_light = -1;
-var current_direction = "RIGHT";
-var light_count = 6;
-var interval_id = null;
+let current_light = -1;
+let current_direction = "RIGHT";
+let light_count = 20;
+let interval_id = null;
+let reset = {
+    "background": "#ffffff",
+    "box-shadow": "none"
+};
+let on = {
+    "background": "#ff0000",
+    "box-shadow": "0 0 5px #8a0202, 0 0 8px #de0505, 0 0 10px #ee0c0c, 0 0 15px #ee3131"
+};
+let shadow = {
+    "background": "#FF7E7AE5",
+    "box-shadow": "0 0 3px #8a0202, 0 0 5px #de0505, 0 0 7px #ee0c0c, 0 0 10px #ee3131"
+};
 
-const myFunction = ()=> {
-    $('.light').css("background", "white");
+const lighting = ()=> {
+    $('.light').css(reset);
     if(current_direction==="RIGHT"){
         current_light++;
-        $('.light').eq(current_light).css("background", "red");
+        $('.light').eq(current_light).css(on);
         if(current_light>=0){
-            $('.light').eq(current_light-1).css("background", "#FA5F55");
+            $('.light').eq(current_light-1).css(shadow);
         }
-
-        $('.light').eq(current_light+1).css("background", "#FA5F55");
         if(current_light===light_count-1){
             current_direction="LEFT";
         }
     }else{
         current_light--;
-        $('.light').eq(current_light).css("background", "red");
-        $('.light').eq(current_light-1).css("background", "#FA5F55");
-        $('.light').eq(current_light+1).css("background", "#FA5F55");
+        $('.light').eq(current_light).css(on);
+        $('.light').eq(current_light+1).css(shadow);
         if(current_light===0){
             current_direction = "RIGHT";
         }
@@ -28,8 +37,9 @@ const myFunction = ()=> {
 }
 $('#start').on('click', ()=> {
     if(interval_id===null) {
-        interval_id = setInterval(myFunction, 300); /*give function name with time, time ms(1000ms=1s)*/
+        interval_id = setInterval(lighting, 100); /*give function name with time, time ms(1000ms=1s)*/
         $('#audio')[0].play();
+        $('#audio')[0].loop=true;
     }
 });
 $('#stop').on('click', ()=> {
